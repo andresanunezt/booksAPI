d2ad70165a4b92e8c82217d7b6199d760f145ea5
andresanunezt <andresanunez98@gmail.com>
2022-07-22 14:08:15 -0400

    Post Method working

--- app.js
+++ app.js
@@ -5,20 +5,46 @@ const db = mongoose.connect("mongodb://localhost/rest_api_practice");
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel.js");
+const bodyParser = require("body-parser");
 
-bookRouter.route("/books").get((req, res) => {
-  Book.find((err, books) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+bookRouter
+  .route("/books")
+  .post((req, res) => {
+    const book = new Book(req.body);
+
+    book.save();
+    return res.status(201).json(book);
+  })
+  .get((req, res) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  });
+
+bookRouter.use("/books/:bookId", (req, res, next) => {
+  Book.findById(req.params.bookId, (err, book) => {
     if (err) {
       return res.send(err);
     }
-    return res.json(books);
+    if (book) {
+      req.book = book;
+      return next();
+    }
+    return res.sendStatus(404);
   });
 });
 
-// bookRouter.route("/books").get((req, res) => {
-//   const response = { hello: "Hiiiii" };
-//   res.json(response);
-// });
 app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
