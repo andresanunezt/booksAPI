422940b1b715b442b61d1fd76d999981058b2b12
andresanunezt <andresanunez98@gmail.com>
2022-07-22 11:53:44 -0400

    changes

--- bookJson.js
+++ bookJson.js
@@ -1,10 +1,20 @@
 db.books.insert([
   {
     title: "100 Años de Soledad",
+    authoer: "Gabriel Garcia Maquez",
+    genre: "Novel",
     read: false,
   },
   {
     title: "Candide",
+    authoer: "Voltaire",
+    genre: "Satire",
     read: false,
   },
+  {
+    title: "Women",
+    authoer: "Bukowski",
+    genre: "Prose",
+    read: false,
+  }
 ]);
