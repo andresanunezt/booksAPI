d05cec144afbb3773c7c02414284ec3577c911a7
andresanunezt <andresanunez98@gmail.com>
2022-07-22 14:27:37 -0400

    code clean up

--- app.js
+++ app.js
@@ -2,48 +2,15 @@ const express = require(`express`);
 const mongoose = require(`mongoose`);
 const app = express();
 const db = mongoose.connect("mongodb://localhost/rest_api_practice");
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel.js");
 const bodyParser = require("body-parser");
 
+const bookRouter = require('./routes/bookRouter')(Book);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-bookRouter
-  .route("/books")
-  .post((req, res) => {
-    const book = new Book(req.body);
-
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.use("/books/:bookId", (req, res, next) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    if (book) {
-      req.book = book;
-      return next();
-    }
-    return res.sendStatus(404);
-  });
-});
 
 app.use("/api", bookRouter);
 
