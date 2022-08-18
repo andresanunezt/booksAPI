a0c75d876b0336796ab785486b3f53ce9f190068
andresanunezt <andresanunez98@gmail.com>
2022-08-18 17:33:32 -0400

    oauth

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require(`express`);
 const mongoose = require(`mongoose`);
 const app = express();
+const {expressjwt: jwt }= require('express-jwt');
+var jwks = require('jwks-rsa');
 const db = mongoose.connect("mongodb://localhost/rest_api_practice");
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel.js");
 const User = require("./models/userModel.js");
 const bodyParser = require("body-parser");
 
-// const bookRouter = require("./routes/bookRouter")(Book);
 const userRouter = require("./routes/userRouter")(User);
 const signupRouter = require('./routes/signupRouter')(User);
 const loginRouter = require("./routes/loginRouter")(User,Book);
@@ -19,6 +20,20 @@ app.use(bodyParser.json());
 app.use("/api", userRouter);
 app.use('/api', signupRouter);
 app.use("/api", loginRouter);
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://dev-ps5yo02j.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'http://localhost:3000/',
+issuer: 'https://dev-ps5yo02j.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my API LOL");
